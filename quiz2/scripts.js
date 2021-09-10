62821e6b7824251a19f28090ea7a15f2595502cc
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-09 17:40:59 -0700

    Adding nested functions.

---
+++ quiz2/scripts.js
@@ -0,0 +1,16 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton)
+{
+    //declaration
+    function showQuestions(questions, quizContainer){
+        //code
+    }
+
+    function showResults(questions, quizContainer, resultsContainer) {
+
+    }
+
+    //invoke
+    showQuestions 
+
+}
+
